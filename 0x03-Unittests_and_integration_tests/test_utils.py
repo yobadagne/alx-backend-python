a322f62495488c71146bc7201ef0097f891e1035
yobadagne <yobadagne@gmail.com>
2023-07-31 12:01:17 +0000

    3

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -6,7 +6,7 @@ import unittest
 from typing import Dict, Tuple, Union
 from parameterized import parameterized
 from unittest.mock import Mock, patch
-from utils import (access_nested_map , get_json)
+from utils import (access_nested_map , get_json, memoize)
 
 class TestAccessNestedMap(unittest.TestCase):
     """Tests the `access_nested_map` function."""
@@ -38,6 +38,20 @@ class TestGetJson(unittest.TestCase):
         with patch("requests.get", return_value = Mock(**formock)) as req_get:
             self.assertEqual(get_json(test_url),test_payload)
             req_get.assert_called_once_with(test_url)
-
+class TestMemoize(unittest.TestCase):
+    """ Test for memoize"""
+    def test_memoize(self) -> None:
+        """ Test memoize"""
+        class TestClass:
+            def a_method(self):
+                return 42
+            @memoize
+            def a_property(self):
+                return self.a_method()
+        with patch.object(TestClass,"a_method",return_value=lambda: 42) as memo_fxn:
+            test_class = TestClass()
+            self.assertEqual(test_class.a_property(), 42)
+            self.assertEqual(test_class.a_property(), 42)
+            memo_fxn.assert_called_once()
 if __name__ == '__main__':
     unittest.main()
