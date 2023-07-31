be4e69096cc57ce2464e29333ceb0369822ee526
yobadagne <yobadagne@gmail.com>
2023-07-31 07:50:36 +0000

    0

---
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""
+    unit test for nestedmap
+"""
+import unittest
+from typing import Dict, Tuple, Union
+from parameterized import parameterized
+from utils import accsess_nested_map
+
+Class TestAccessNestedMap(unittest.TestCase):
+    """Tests the `access_nested_map` function."""
+    @parameterized.expand([
+        ({"a": 1}, ("a",), 1),
+        ({"a": {"b": 2}}, ("a",), {"b": 2}),
+        ({"a": {"b": 2}}, ("a", "b"), 2),
+    ])
+    def test_access_nested_map(
+            self,
+            nested_map: Dict,
+            path: Tuple[str],
+            expected: Union[Dict, int],
+            ) -> None:
+        """Tests `access_nested_map`'s output."""
+        self.assertEqual(access_nested_map(nested_map, path), expected)
