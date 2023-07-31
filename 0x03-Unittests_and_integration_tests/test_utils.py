bf97cba8ae4a9cdc91c956919443bc5e389030df
yobadagne <yobadagne@gmail.com>
2023-07-31 12:04:27 +0000

    3

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -48,10 +48,10 @@ class TestMemoize(unittest.TestCase):
             @memoize
             def a_property(self):
                 return self.a_method()
-        with patch.object(TestClass,"a_method",return_value=lambda: 42) as memo_fxn:
+        with patch.object(TestClass,"a_method",return_value=lambda: 42) as memo:
             test_class = TestClass()
             self.assertEqual(test_class.a_property(), 42)
             self.assertEqual(test_class.a_property(), 42)
-            memo_fxn.assert_called_once()
+            memo.assert_called_once()
 if __name__ == '__main__':
     unittest.main()
