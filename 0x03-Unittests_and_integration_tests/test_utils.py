1e5133140e5d376bdd74f019b1c67f1b20526e83
yobadagne <yobadagne@gmail.com>
2023-07-31 08:26:45 +0000

    1

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -14,11 +14,18 @@ class TestAccessNestedMap(unittest.TestCase):
         ({"a": {"b": 2}}, ("a",), {"b": 2}),
         ({"a": {"b": 2}}, ("a", "b"), 2),
     ])
-    def test_access_nested_map(
-            self,
-            nested_map: Dict,
-            path: Tuple[str],
-            expected: Union[Dict, int],
-            ) -> None:
+    def test_access_nested_map(self, nested_map: Dict, path: Tuple[str], expected: Union[Dict, int]) -> None:
         """Tests `access_nested_map`'s output."""
         self.assertEqual(access_nested_map(nested_map, path), expected)
+    @parameterized.expand([
+        ({}, ("a", ),KeyError),
+        ({"a": 1}, ("a","b"), KeyError),
+        ])
+    def test_access_nested_map_exception(self, nested_map:Dict, path: Tuple[str], exception: Exception) -> None:
+        """ Test for Exception"""
+        with self.assertRaises(exception):
+            access_nested_map(nested_map, path)
+
+
+if __name__ == '__main__':
+    unittest.main()
